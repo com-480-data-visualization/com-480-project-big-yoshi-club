a2e740fdc21dbed20cc63208816cdf3a29f99b4e
Loïc Droz <lk.droz@gmail.com>
2020-04-26 00:38:14 +0200

    Create project file structure and create hello world with express

---
+++ website/server.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Imports
+const express = require('express')
+const path = require('path')
+
+// Instantiate app and choose port
+const app = express()
+const port = 3000
+
+// Setup routing
+app.use('/html', express.static('html'))
+app.use('/js', express.static('js'))
+app.use('/css', express.static('css'))
+
+// Serve main HTML file
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'html', 'index.html'))
+})
+
+// Start listening for requests
+app.listen(port, () => console.log(`Website listening at http://localhost:${port}`))
