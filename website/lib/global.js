787d4900c6642f9510ae670a8423e9aa8de59359
Moodoo245 <justinas.sukaitis@epfl.ch>
2020-05-06 17:11:13 +0200

    let the tables hit the floor

--- website/lib/global.js
+++ website/lib/global.js
@@ -4,7 +4,10 @@ class Yoshi{
         this.data
         this.svg_map = map_svg
         this.roll_svgs = roll_svgs
-
+        this.projection_style = d3.geoNaturalEarth1()
+        
+        d3.select('#projection-dropdown')
+            .on('click', () => this.projection_select())
         let a = data[0][0]['Last Known Eruption']
         let b = data[1][0]['Date']
         let c = data[2][0]['year']
@@ -13,10 +16,43 @@ class Yoshi{
         console.log(b)
         console.log(c)
 
-        this.map = new Map(data)
+        this.map = new Map('map', this.data, this.projection_style)
 
         this.volcano_roll = new Roll(data[0], roll_svgs[0], 'volcanoes', 'Last Known Eruption')
         this.earthquakes_roll = new Roll(data[1], roll_svgs[1], 'earthquakes', 'Date')
         this.meteores_roll = new Roll(data[2], roll_svgs[2], 'meteors', 'year')
+
+        
     }
+
+    projection_select(){
+        
+        d3.select('#projection-dropdown')
+            
+            .on('click', () => menu())        
+
+       
+    }
+
+    
 }
+
+function menu(){
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    
+    if (event.target.id === "Gnomonic"){
+        this.projection_style = d3.geoGnomonic()
+        
+        this.map = new Map('map', this.data, this.projection_style)
+        
+    }
+    else if (event.target.id == "Natural"){
+        this.projection_style = d3.geoNaturalEarth1()
+        this.map = new Map('map', this.data, this.projection_style)
+    }
+    
+  } 
--- website/lib/map.js
+++ website/lib/map.js
@@ -1,21 +1,24 @@
 
 
 class Map{
-    constructor(svg_element_id){
+    constructor(svg_element_id, data, projection_style){
         this.svg = d3.select('#' + svg_element_id);
         const svg_viewbox = this.svg.node().viewBox.animVal;
 		this.svg_width = svg_viewbox.width;
         this.svg_height = svg_viewbox.height;
-        
+        this.svg.append('rect')
+                    .attr('fill', 'rgb(140,30,30)')
+                    .attr('width', `${this.svg_width}`)
+                    .attr('height', `${this.svg_height}`)
 
         function silly_color(name) {
             if (name[0] == "A"){
-                return d3.color("red")
+                return d3.color("rgb(255, 0,0)")
             }else{
-                return d3.color("blue")
+                return d3.color("rgb(205, 0,10)")
             }
         }
-        const projection = d3.geoNaturalEarth1()
+        const projection = projection_style
 							.rotate([0,0])
 							.center([0,0])
 							.scale(200)
@@ -35,6 +38,7 @@ class Map{
             let map_data = results[0];
             
             
+
             this.map_container = this.svg.append('g');
             this.map_container.selectAll(".country")
                                 .data(map_data)
@@ -50,7 +54,7 @@ class Map{
 
     
 }
-
+/*
 function whenDocumentLoaded(action) {
 	if (document.readyState === "loading") {
 		document.addEventListener("DOMContentLoaded", action);
@@ -63,3 +67,4 @@ whenDocumentLoaded(() => {
 	plot_object = new Map('map');
 	// plot object is global, you can inspect it in the dev-console
 });
+*/
