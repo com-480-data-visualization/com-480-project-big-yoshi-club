2788cacc53c7ed272e3b74059cda02ff1a458e5e
Leo Serena <leoserena@hotmail.ch>
2020-05-06 16:39:08 +0200

    time

--- website/lib/global.js
+++ website/lib/global.js
@@ -2,9 +2,25 @@ class Yoshi{
 
     constructor(data, map_svg, roll_svgs){
         this.data = data
-        this.svg_map = map_svg
+        this.map_svg = map_svg
         this.roll_svgs = roll_svgs
+
+        let oldest_v = d3.max(this.data[0], v => v['Last Known Eruption'])
+        let oldest_e = d3.max(this.data[1], e => e['Date'])
+        let oldest_m = d3.max(this.data[2], m => m['year'])
+        this.oldest = d3.max([oldest_v, oldest_e, oldest_m])
+
+        let youngest_v = d3.min(this.data[0], v => v['Last Known Eruption'])
+        let youngest_e = d3.min(this.data[1], e => e['Date'])
+        let youngest_m = d3.min(this.data[2], m => m['year'])
+        this.youngest = d3.min([youngest_v, youngest_e, youngest_m])
+
+        console.log(this.oldest)
+        console.log(this.youngest)
+        //time management
         this.on = false
+        //this.time_window = 50
+        //this.year0 = this.oldest
 
         //print examples for each data
         let a = data[0][0]
@@ -16,25 +32,32 @@ class Yoshi{
 
         //this.map = new Map(map_svg, data)
 
-        let oldest_v = d3.max(this.data[0], v => v['Last Known Eruption'])
-        let oldest_e = d3.max(this.data[1], e => e['Date'])
-        let oldest_m = d3.max(this.data[2], m => m['year'])
-
-        this.oldest = d3.max([oldest_v, oldest_e, oldest_m])
-        console.log(this.oldest)
-
         this.volcano_roll = new Roll(this, data[0], roll_svgs[0], 'V', 'Last Known Eruption')
         this.earthquakes_roll = new Roll(this, data[1], roll_svgs[1], 'E', 'Date')
         this.meteores_roll = new Roll(this, data[2], roll_svgs[2], 'M', 'year')
     }
 
 
+    //button functionalities
+    start(){
+        this.on = true
+        while(this.on){
+            this.tick()
+        }
+    }
+
+    stop(){
+        this.on = false
+        //draw static points
+    }
+
+    reset(){
+        this.on = false
+        //draw static points
+    }
 
     tick(){
         //update map
         //update rolls
-        if(this.on){
-            this.tick()
-        }
     }
 }
