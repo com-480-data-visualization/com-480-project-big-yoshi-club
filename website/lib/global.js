05323da3c54b90d2590d7685221f1db1fcfa3a56
Loïc Droz <lk.droz@gmail.com>
2020-05-06 16:13:33 +0200

    Add time controls display function

--- website/lib/global.js
+++ website/lib/global.js
@@ -9,5 +9,14 @@ class Yoshi{
         this.volcano_roll = new Roll(data[0], roll_svgs[0])
         this.earthquakes_roll = new Roll(data[1], roll_svgs[1])
         this.meteores_roll = new Roll(data[2], roll_svgs[2])
+
+        // Add timeline controls and display
+        const svgId = "#time-controls"
+        const minDate = new Date(860, 1, 1)
+        const maxDate = new Date(2020, 1, 1)
+        const twLoBnd = new Date(1245, 1, 1)
+        const twUpBnd = new Date(1963, 1, 1)
+        this.timelineControl = new TimelineControl(svgId, minDate, maxDate, twLoBnd, twUpBnd)
+        this.timelineControl.display()
     }
 }
